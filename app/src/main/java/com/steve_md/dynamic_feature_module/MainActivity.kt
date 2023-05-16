cfadc4e14b5e73a26db420b3250f8b1bdaf9effa
MuindiStephen <stephenmuindi24@gmail.com>
2023-05-16 21:39:54 +0300

    adds; app level design

--- app/src/main/java/com/steve_md/dynamic_feature_module/MainActivity.kt
+++ app/src/main/java/com/steve_md/dynamic_feature_module/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.steve_md.dynamic_feature_module
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.google.android.play.core.splitinstall.SplitInstallRequest
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val mySessionId:Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +29,49 @@ class MainActivity : AppCompatActivity() {
             .newBuilder()
             .addModule("dynamic-feature-test")
             .build()
+
+        val listener =
+            SplitInstallStateUpdatedListener { splitInstallSessionState ->
+                if (splitInstallSessionState.sessionId() == mySessionId) {
+                    when (splitInstallSessionState.status()) {
+                        SplitInstallSessionStatus.INSTALLED -> {
+                            Log.d(TAG, "Dynamic Module downloaded")
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Dynamic Module downloaded",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        SplitInstallSessionStatus.CANCELED -> {
+                            //Log.d("Dynamic module concelled", "")
+                        }
+                        SplitInstallSessionStatus.CANCELING -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.DOWNLOADED -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.DOWNLOADING -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.FAILED -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.INSTALLING -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.PENDING -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.REQUIRES_USER_CONFIRMATION -> {
+                            TODO()
+                        }
+                        SplitInstallSessionStatus.UNKNOWN -> {
+                            TODO()
+                        }
+                    }
+                }
+            }
 
     }
 
---
+++ dynamic-feature-test/src/main/java/com/steve_md/dynamic_feature_test/DynamicTestMainActivity.kt
@@ -0,0 +1,11 @@
+package com.steve_md.dynamic_feature_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DynamicTestMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamic_test_main)
+    }
+}
