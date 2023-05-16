2e2d3e5c37526d3721b7cf74d3602dc342dc6bee
MuindiStephen <stephenmuindi24@gmail.com>
2023-05-16 19:10:34 +0300

    dynamic-feature-test

--- app/src/main/java/com/steve_md/dynamic_feature_module/MainActivity.kt
+++ app/src/main/java/com/steve_md/dynamic_feature_module/MainActivity.kt
@@ -2,10 +2,26 @@ package com.steve_md.dynamic_feature_module
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        downloadDynamicModule()
     }
+
+    private fun downloadDynamicModule() {
+        val splitInstallManager:SplitInstallManager = SplitInstallManagerFactory.create(this)
+        val splitInstallRequest:SplitInstallRequest = SplitInstallRequest
+            .newBuilder()
+            .addModule("dynamic-feature-test")
+            .build()
+
+    }
+
+
 }
